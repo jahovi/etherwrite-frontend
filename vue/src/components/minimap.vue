91fc879ed0cb70d28ca9ae4330f8fc4ff2887c78
Marie <marie_freise@web.de>
2022-11-27 18:10:15 +0000

    33: added dynamic user positions to minimap

--- vue/src/components/minimap.vue
+++ vue/src/components/minimap.vue
@@ -3,14 +3,41 @@
 <!-- @copyright Marie Freise, 2022, marie_freise@web.de -->
 
 <template>
-	<div class="minimap" v-dragscroll.y="true">
-    <span
-				class="textBlock"
-				:style="{ background: block.color }"
-				v-for="block in coloredBlocks"
-				:key="block.id"
-		>{{ block.content }}</span>
-	</div>
+  <div>
+    <div class="scrollbox scrollbox_delayed" v-dragscroll.y="true">
+      <div class="minimap" ref="minimapRef">
+        <div class="outerContainer">
+          <div class="scrollViewContainer">
+            <div
+              class="scrollView"
+              v-for="userPos in userPositions"
+              :key="userPos.id"
+              :style="{
+                background: userPos.color,
+                top: userPos.top + 'px',
+              }"
+            ></div>
+          </div>
+          <div class="textBlockContainer" ref="textBlocks">
+            <span
+              :class="block.headingType"
+              v-for="block in coloredBlocks"
+              :key="block.id"
+              class="textBlock"
+              :style="{
+                background: block.color,
+                color:
+                  block.headingType == 'h1' && block.color == 'transparent'
+                    ? 'rgb(100,210,155)'
+                    : 'black',
+              }"
+              >{{ block.content }}</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,6 +50,9 @@ export default {
 			keepRefreshing: true,
 			coloredBlocks: [],
 			authorData: {},
+			blockInfo: [],
+			scrollPos: {},
+			userPositions: []
 		};
 	},
 	directives: {
@@ -50,30 +80,76 @@ export default {
 				Communication.getFromEVA("getAuthorInfo")
 						.then(authors => this.authorData = authors);
 
-				//Cross-origin requests need to be safe
-				const data = await Communication.getFromEVA("getBlockInfo", {
+				Communication.getFromEVA("getBlockInfo", {
 					padName: this.padName,
-				});
-				this.processMinimapContent(data);
+				}).then(blockInfo => this.blockInfo = blockInfo);
 
-				await sleep(1000);
+				Communication.getFromEVA("getScrollPos",{
+					padName: this.padName,
+				}).then(scrollPos => this.scrollPos = scrollPos);
+
+				this.processTextBlocks();
+				this.processScrollPos();
+
+				await sleep(100);
 			}
 		},
-		processMinimapContent: async function (data) {
+		processScrollPos: function () {
 
+			this.userPositions = [];
+
+			for (const [authorId, scrollPosInfo] of Object.entries(this.scrollPos)) {
+				const textBlocks = this.$refs.textBlocks.children;
+
+				if (textBlocks.length > scrollPosInfo.topIndex-1) {
+					const authorColor = this.authorData[authorId]["color"];
+
+					const minimapRef = this.$refs.minimapRef;
+
+					const topPosition = textBlocks[scrollPosInfo.topIndex-1]
+						.getBoundingClientRect().top - minimapRef.getBoundingClientRect().top; //Use the top of the minimap as an anchor reference point to position the viewport of the users
+					
+					this.userPositions.push({id: this.userPositions.length, top: topPosition, color: authorColor});
+				}
+			}
+		},
+		processTextBlocks: function () {
+			
 			this.coloredBlocks = [];
+			let isHeading = false;
+			let currentHeadingType = "normal";
+			const sampleText = "Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua At vero eos et accusam et justo duo dolores et ea rebum Stet clita kasd gubergren no sea takimata sanctus est Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua At vero eos et accusam et justo duo dolores et ea rebum Stet clita kasd gubergren no sea takimata sanctus est Lorem ipsum dolor sit amet";
 
-			data.forEach((block) => {
+			this.blockInfo.forEach((block) => {
 				let rndContent = "";
-				const blockColor = this.authorData[block.author]["color"]; //Getting the author's color for the current block.
+				const blockColor = block.ignoreColor ? "transparent" : this.authorData[block.author]["color"]; //Getting the author's color for the current block
+				let sampleIndex = 0;
 				for (var i = 0; i < block.blockLength; i++) {
-					if (block.lineBreakIndices && block.lineBreakIndices.includes(i)) {
-						rndContent += "\n";
+
+					if(block.headingStartIndices && block.headingStartIndices.includes(i)) {
+						rndContent += "";
+						isHeading = true;
+						currentHeadingType = block.headingTypes[i];
+					}
+					else if (block.lineBreakIndices && block.lineBreakIndices.includes(i)) {
+						rndContent += isHeading ? "\n\n" : "\n";
+						this.coloredBlocks.push({id: this.coloredBlocks.length, color: blockColor, content: rndContent, headingType: currentHeadingType});
+						sampleIndex = 0;
+						isHeading = false;
+						rndContent = "";
+						currentHeadingType = "normal";
+						
 					} else {
-						rndContent += "c";
+
+						if (sampleIndex >= sampleText.length) {
+							sampleIndex = 0;
+						}
+						rndContent += sampleText[sampleIndex];
+						sampleIndex++;
 					}
 				}
-				this.coloredBlocks.push({id: this.coloredBlocks.length, color: blockColor, content: rndContent});
+				this.coloredBlocks.push({id: this.coloredBlocks.length, color: blockColor, content: rndContent, headingType: currentHeadingType});
+				sampleIndex = 0;
 			});
 		},
 	},
@@ -81,29 +157,89 @@ export default {
 </script>
 
 <style scoped>
-.textBlock {
-	white-space: pre-line;
+.scrollbox {
+  visibility: hidden;
+  position: absolute;
+  float: left;
+  width: 10%;
+  height: 660px;
+  top: 40px;
+  bottom: 40px;
+  font-size: 2px;
+  overflow: auto;
+  overflow-wrap: break-word;
+  scrollbar-width: thin;
+}
+
+.scrollbox:hover {
+  visibility: visible;
+}
+
+.scrollbox_delayed {
+  transition: visibility 0.2s;
+}
+.scrollbox_delayed:hover {
+  transition: visibility 0s 0.2s;
 }
 
 .minimap {
-	background-color: rgba(0, 0, 0, 0.05);
-	position: absolute;
-	float: left;
-	width: 10%;
-	height: 620px;
-	top: 40px;
-	bottom: 40px;
-	font-size: 3px;
-	padding: 10px;
-	overflow-wrap: break-word;
-	overflow: auto;
-	-ms-overflow-style: none; /* Hide Scrollbar Internet Explorer 10+ */
-	scrollbar-width: none; /* Hide Scrollbar Firefox */
-	cursor: grab;
-	user-select: none;
+  background-color: rgba(0, 0, 0, 0.05);
+  visibility: visible;
+  min-height: 660px;
+  user-select: none;
+  overflow: auto;
+  overflow-wrap: break-word;
 }
 
-.minimap::-webkit-scrollbar {
-	display: none; /* Hide Scrollbar Safari and Chrome */
+.outerContainer {
+  display: flex;
+}
+
+.scrollViewContainer {
+  height: 100%;
+  width: 100%;
+  z-index: 10;
+}
+
+.scrollView {
+  margin: 0px;
+  float: left;
+  padding: 0px;
+  width: 100%;
+  height: 90px;
+  opacity: 0.5;
+  position: absolute;
+}
+
+.textBlockContainer {
+  width: 100%;
+  margin-left: -75%;
+  margin-top: 10%;
+  margin-right: 10%;
+  margin-bottom: 10%;
+}
+
+.textBlock {
+  white-space: pre-line;
+}
+
+.h1 {
+  font-weight: 900;
+  font-size: 6px;
+}
+
+.h2 {
+  font-weight: 700;
+  font-size: 4px;
+}
+
+.h3 {
+  font-weight: 500;
+  font-size: 3px;
+}
+
+.h4 {
+  font-weight: 300;
+  font-size: 2.5px;
 }
 </style>
