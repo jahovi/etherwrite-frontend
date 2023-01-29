8800f6875152655a84884607bbc0265306e14192
Marie <marie_freise@web.de>
2023-01-29 19:39:07 +0000

    80 improved authoringRatios_bar chart

--- vue/src/components/charts/authoringRatios_bar.vue
+++ vue/src/components/charts/authoringRatios_bar.vue
@@ -2,17 +2,17 @@
   <div>
     <h4>Schreibanteil pro Person</h4>
     <div class="chart-container">
-      <ul class="legend mt-3" v-if="authors.length" style="flex-shrink: 0">
-        <li v-for="author in authors" :key="author">
-          {{ author
-          }}<i class="fa fa-square" :style="{ color: colorFn(author) }"></i>
-        </li>
-      </ul>
       <div
         class="chart"
         style="width: 100%; height: 100%"
         :id="elementId"
       ></div>
+      <ul class="legend mt-3" v-if="authors.length" style="flex-shrink: 0">
+        <li v-for="author in authors" :key="author">
+          <i class="fa fa-square" :style="{ color: colorFn(author) }"></i>
+          {{ author }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -28,12 +28,16 @@ export default {
 			authors: [],
 			ratios: [],
 			colors: [],
+			widthOfSvg: 800,
+			heightOfSvg: 200,
 		};
 	},
 	props: {
 		id: String,
 		isMock: Boolean,
 		padName: String,
+		w: Number,
+		h: Number,
 	},
 	computed: {
 		elementId() {
@@ -60,6 +64,14 @@ export default {
 		this.$emit("dashboardDimensions", this.getDashboardDimensions);
 	},
 	watch: {
+		w(val) {
+			this.widthOfSvg = val;
+			this.loadBar();
+		},
+		h(val) {
+			this.heightOfSvg = val;
+			this.loadBar();
+		},
 		padName() {
 			if (!this.isMock) {
 				this.getData().then(() =>
@@ -69,13 +81,14 @@ export default {
 	},
 	methods: {
 		getDashboardDimensions() {
-			return {w: 4, h: 8};
+			return {w: 7, h: 12};
 		},
 		async getData() {
 			return Communication.getFromEVA("authoring_ratios", { pad: this.padName })
 				.then(data => {
+					const base = data.ratios.reduce((e, result) => e+result);
 					this.authors = data.authors;
-					this.ratios = data.ratios;
+					this.ratios = data.ratios.map(e => e/base*100);
 					this.colors = data.colors;
 				})
 				.catch(() => {
@@ -83,11 +96,10 @@ export default {
 				});
 		},
 		loadBar() {
-			document.getElementById(this.elementId).childNodes.forEach(c => c.remove());
-			
+
 			if (this.ratios.length === 0) {
-        return;
-      }
+				return;
+			}
 			// Add a filler if the given numbers don't add up to 100%.
 			// Can happen with weird test data.
 			const sum = this.ratios.reduce((result, entry) => result + entry, 0);
@@ -97,47 +109,108 @@ export default {
 				this.colors = [...this.colors, "#ccc"];
 			}
 
-			const svg = d3.select(`#${this.elementId}`);
+			d3.select("#authoringRatiosBar-svg").remove();
 
-			const chart = svg.append("bar")
-				.attr("class", "bar");
+			const data = [];
+			for (let i = 0; i < this.ratios.length; i++) {
+				data.push({
+					author: this.authors[i],
+					ratio: this.ratios[i],
+					color: this.colors[i],
+				});
+			}
 
-			//Eigenschaften der Balken festlegen	
-			const arcs = chart.selectAll("arc")
-				.data(this.ratios)
-				.enter().append("div")
-				.style("width", d => d + "%")
-				.style("background-color", (i) => this.colorFn(i))
-				.style("overflow", "visible");
+			// set the dimensions and margins of the graph
+			var margin = {top: 25, right: 0, bottom: 50, left: 150},
+				width = this.widthOfSvg * 0.6 - margin.left - margin.right,
+				height = this.heightOfSvg - margin.top - margin.bottom;
 
-			//Text auf den Balken mit dem jeweiligen Wert
-			arcs.append("span")
-				.attr("class", "chart-label")
-				.text(d => `${d} %`);
+			// append the svg object to the body of the page
+			const svg = d3.select(`#${this.elementId}`)
+				.append("svg")
+				.attr("id", "authoringRatiosBar-svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform",
+					"translate(" + margin.left + "," + margin.top + ")");
 
-		},
+			// Add X axis
+			var x = d3.scaleLinear()
+				.domain([0, 100])
+				.range([ 0, width]);
+
+			const xAxis = d3.axisBottom(x);
+
+			svg.append("g")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis)
+				.selectAll("text")
+				.style("font-family", "sans-serif")
+				.style("font-size", "12px")
+				.attr("transform", "translate(-10,0)rotate(-45)")
+				.style("text-anchor", "end");
+
+			// Y axis
+			var y = d3.scaleBand()
+				.range([ 0, height ])
+				.domain(data.map(function(d) { return d.author; }))
+				.padding(.1);
+			
+			const yAxis = d3.axisLeft(y);
+			
+			svg.append("g")
+				.call(yAxis)
+				.selectAll("text")
+				.style("font-family", "sans-serif")
+				.style("font-size", "12px");
+
+			// Bars
+			svg.selectAll("myRect")
+				.data(data)
+				.enter()
+				.append("rect")
+				.attr("x", x(0) )
+				.attr("y", function(d) { return y(d.author); })
+				.attr("width", function(d) { return x(d.ratio); })
+				.attr("height", y.bandwidth() )
+				.attr("fill", d => d.color);
+
+			// X-axis labels
+			svg.append("text")
+				.attr("text-anchor", "middle")
+				.style("font-weight", "bold")
+				.attr("transform", "translate("+ (width/2) + "," +(height+(margin.bottom * 0.90)) + ")")
+				.text("Schreibanteil in Prozent");
+			
+				
+			// Horizontal bar labels
+			svg.append("g")
+				.attr("transform", "translate(5, 0)")
+				.selectAll(".text")
+				.data(data)
+				.enter()
+				.append("text")
+				.style("font-size", "12px")
+				.style("font-family", "sans-serif")
+				.attr("x", d => x(d.ratio))
+				.attr("y", d => y(d.author) + y.bandwidth()/2)
+				.text(d => d.ratio.toFixed(2) + "%"); 
+
 	},
+	}
 };
+
 </script>
-<style lang="css">
+<style scoped>
 .chart-container {
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
+  justify-content: left;
   align-items: center;
-  gap: 20px;
 }
 
 .chart-container .bar {
   text-align: right;
-}
-
-.chart-label {
-  background-color: rgba(255, 255, 255, 0.6);
-  color: black;
-  padding: 2px;
-  font: 10px sans-serif;
-  white-space: nowrap;
-  vertical-align: middle;
 }
 </style>
