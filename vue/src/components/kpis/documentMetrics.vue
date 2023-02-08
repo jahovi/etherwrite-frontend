a8b53fba2c57856317606480e9422f34c508688b
Giacomo Voß <giacomo.voss@studium.fernuni-hagen.de>
2023-02-08 20:41:50 +0100

    Document metrics fixed

--- vue/src/components/kpis/documentMetrics.vue
+++ vue/src/components/kpis/documentMetrics.vue
@@ -11,48 +11,54 @@
 import Communication from "../../classes/communication";
 
 export default {
-  name: "documentMetrics",
-  props: {
-    padName: String,
-  },
-  data() {
-    return {
-      ws: null,
-      numChars: 0,
-      numWords: 0,
-    };
-  },
-  computed: {
-    elementId() {
-      return `documentMetrics_${this.id}`;
-    },
-    getStrings() {
-      return this.$store.getters.getStrings;
-    },
-  },
-  mounted() {
-    this.openSocket();
-  },
-  unmounted() {
-    this.closeSocket();
-  },
-  methods: {
-    openSocket() {
-      this.ws = Communication.openSocket("wstest", {
-        padName: this.padName,
-      });
-      this.ws.on("update", (msg) => {
-        this.numChars = msg[0];
-        this.numWords = msg[1];
-      });
-    },
-    closeSocket() {
-      this.ws.disconnect();
-    },
-    getDashboardDimensions() {
-      return {w: 3, h: 6};
-    },
-  },
+	name: "documentMetrics",
+	props: {
+		padName: String,
+	},
+	data() {
+		return {
+			ws: null,
+			numChars: 0,
+			numWords: 0,
+		};
+	},
+	computed: {
+		elementId() {
+			return `documentMetrics_${this.id}`;
+		},
+		getStrings() {
+			return this.$store.getters.getStrings;
+		},
+	},
+	mounted() {
+		this.openSocket();
+	},
+	unmounted() {
+		this.closeSocket();
+	},
+	methods: {
+		openSocket() {
+			this.ws = Communication.openSocket("wstest", {
+				padName: this.padName,
+			});
+			this.ws.on("update", (msg) => {
+				this.numChars = msg[0];
+				this.numWords = msg[1];
+			});
+		},
+		closeSocket() {
+			this.ws.disconnect();
+		},
+		getDashboardDimensions() {
+			return {w: 3, h: 6};
+		},
+	},
+	watch: {
+		padName() {
+			this.closeSocket();
+			this.openSocket();
+		},
+	},
 };
 </script>
 
